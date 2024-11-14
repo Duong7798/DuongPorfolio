50077c2dee87eeac76dd6d58b7a7c6e7e81c0088
Duong7798 <ntdconin@gmail.com>
2024-11-14 20:58:13 +0700

    Add files via upload

---
+++ js/JAVA custom.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["I'm a Freelance", "I'm a Freelance", "I'm working on Graphic Design"],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
+
